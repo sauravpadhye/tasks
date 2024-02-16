b0203018f46ece93d4bf97b78ff83ae82b569369
Saurav Padhye <spadhye@udel.edu>
2024-02-15 20:04:12 -0500

    finished all parts of task 3 except two column

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
+import img from "./Images/UDLogo.jpg";
 
 function App(): JSX.Element {
     return (
@@ -12,12 +14,15 @@ function App(): JSX.Element {
                 automatically reload. Hello World
             </p>
             <h2>This is a new header</h2>
-            <img src="./Images/UDLogo.jpg" />
+            <img src={img} alt="UD Logo" />
             <ul>
                 <li>First list item</li>
                 <li>Second list item</li>
                 <li>Third list item</li>
             </ul>
+            <Button onClick={() => console.log("Hello world!")}>
+                Log Hello World
+            </Button>
         </div>
     );
 }
