0270987cc21c049d06190eff3b46491acfe5bd5b
Saurav Padhye <spadhye@udel.edu>
2024-02-27 18:26:15 -0500

    finished publishQuesstion

--- src/objects.ts
+++ src/objects.ts
@@ -108,7 +108,8 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    const pubQ = { ...question, published: !question.published };
+    return pubQ;
 }
 
 /**
