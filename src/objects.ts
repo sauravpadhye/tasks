905e5ba8ccf8d59f53db760e8c2bd5e61369e8fa
Saurav Padhye <spadhye@udel.edu>
2024-02-27 18:09:44 -0500

    finished toMarkdown

--- src/objects.ts
+++ src/objects.ts
@@ -83,7 +83,14 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    let form = "# " + question.name + "\n" + question.body;
+    if (question.type === "multiple_choice_question") {
+        form += "\n";
+        question.options.forEach((option) => {
+            form += `- ${option}\n`;
+        });
+    }
+    return form.trimEnd();
 }
 
 /**
