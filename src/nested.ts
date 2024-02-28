afeb27d89a1ce980e9aa973bd43383897d155e0e
Saurav Padhye <spadhye@udel.edu>
2024-02-27 20:32:49 -0500

    finished add new quesion

--- src/nested.ts
+++ src/nested.ts
@@ -1,5 +1,6 @@
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
+import { makeBlankQuestion } from "./objects";
 
 /**
  * Consumes an array of questions and returns a new array with only the questions
@@ -154,7 +155,8 @@ export function addNewQuestion(
     name: string,
     type: QuestionType
 ): Question[] {
-    return [];
+    const added = questions.concat(makeBlankQuestion(id, name, type));
+    return added;
 }
 
 /***
