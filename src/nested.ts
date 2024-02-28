50f2ae267dc2387b429fa2fc1aacb6775094669f
Saurav Padhye <spadhye@udel.edu>
2024-02-27 20:31:01 -0500

    finished same type

--- src/nested.ts
+++ src/nested.ts
@@ -138,7 +138,9 @@ export function publishAll(questions: Question[]): Question[] {
  * are the same type. They can be any type, as long as they are all the SAME type.
  */
 export function sameType(questions: Question[]): boolean {
-    return false;
+    const type = questions[0]?.type;
+    const flag = questions.every((question) => question.type === type);
+    return flag;
 }
 
 /***
