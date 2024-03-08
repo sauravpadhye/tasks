e36380129dec3593774692264f55a3b07d787779
Saurav Padhye <spadhye@udel.edu>
2024-03-07 20:40:25 -0500

    finished mcq

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -7,9 +7,29 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [choice, setChoice] = useState(options[0]);
+    const [isCorrect, setIsCorrect] = useState(false);
+    const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const selected = e.target.value;
+        setChoice(selected);
+        if (selected === expectedAnswer) {
+            setIsCorrect(true);
+        } else {
+            setIsCorrect(false);
+        }
+    };
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <select value={choice} onChange={handleSelect}>
+                {options.map((op, ind) => (
+                    <option key={ind} value={op}>
+                        {op}
+                    </option>
+                ))}
+            </select>
+            {isCorrect === true && <span>✔️</span>}
+            {isCorrect === false && <span>❌</span>}
         </div>
     );
 }
